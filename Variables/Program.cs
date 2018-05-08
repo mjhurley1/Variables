cb4b5fb7d633e8ef41e398781432bce87bd81cdb
unknown <mhurley@hurley-consulting.io>
2018-05-08 13:51:24 -0400

    name variable

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,6 +10,7 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            /*
             int x;
             int y;
 
@@ -18,6 +19,18 @@ namespace Variables
 
             Console.WriteLine(y);
             Console.ReadLine();
+            */
+
+            Console.WriteLine("what is your name");
+            Console.Write("type your first name" );
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type tyour last name" );
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
 
 
         }
